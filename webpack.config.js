3a8cb509fe9c7ecb12632fa787e24d0ea5e7c0b7
Unknown <cbodasing@gmail.com>
2017-09-07 19:48:46 +0200

    feature: Clean up unused dependencies and files

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,14 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: './src/index.js',
   resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    extensions: ['.js'],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         include: [path.resolve(__dirname, 'src')],
         exclude: [path.resolve(__dirname, 'node_modules')],
         loader: 'babel-loader',
@@ -22,14 +16,4 @@ module.exports = {
     ],
   },
   devtool: 'source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    port: 8080,
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-    }),
-  ],
 };
